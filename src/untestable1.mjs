d561cc7f28c1514947fa9e3a19afa218539254c5
hughbuntine <hbuntine@student.unimelb.edu.au>
2025-02-13 16:39:06 +0200

    1st one done

--- src/untestable1.mjs
+++ src/untestable1.mjs
@@ -1,9 +1,19 @@
 const millisPerDay = 24 * 60 * 60 * 1000;
 
-export function daysUntilChristmas() {
-  const now = new Date();
-  const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-  const christmasDay = new Date(now.getFullYear(), 12 - 1, 25);
+function daysUntilChristmasBad() {
+  const now = new Date(); // Uses the current date and time so it's untestable
+  const today = new Date(now.getFullYear(), now.getMonth(), now.getDate()); // Uses the current date so it's untestable
+  const christmasDay = new Date(now.getFullYear(), 12 - 1, 25); // finds the next Christmas so it's untestable or you'd have to update it each year
+  if (today.getTime() > christmasDay.getTime()) {
+    christmasDay.setFullYear(new Date().getFullYear() + 1);
+  }
+  const diffMillis = christmasDay.getTime() - today.getTime();
+  return Math.floor(diffMillis / millisPerDay);
+}
+
+export function daysUntilChristmas(moment) {
+  const today = new Date(moment.getFullYear(), moment.getMonth(), moment.getDate()); 
+  const christmasDay = new Date(moment.getFullYear(), 12 - 1, 25); 
   if (today.getTime() > christmasDay.getTime()) {
     christmasDay.setFullYear(new Date().getFullYear() + 1);
   }
--- test/untestable1.test.mjs
+++ test/untestable1.test.mjs
@@ -3,8 +3,22 @@ import { expect } from "chai";
 import { daysUntilChristmas } from "../src/untestable1.mjs";
 
 describe("Untestable 1: days until Christmas", () => {
-  test("todo", () => {
+  test("Feb 13 2025", () => {
+    let testDate = new Date("2025-02-13T14:30:37.295Z");
     // TODO: write proper tests
-    expect(daysUntilChristmas()).to.be.a("number");
+    expect(daysUntilChristmas(testDate)).to.equal(315);
   });
+
+  test("Dec 25 2025", () => {
+    let testDate = new Date(2025, 11, 25);
+    // TODO: write proper tests
+    expect(daysUntilChristmas(testDate)).to.equal(0);
+  });
+
+  test("Dec 26 2025", () => {
+    let testDate = new Date(2025, 11, 26);
+    // TODO: write proper tests
+    expect(daysUntilChristmas(testDate)).to.equal(364);
+  });
+
 });
